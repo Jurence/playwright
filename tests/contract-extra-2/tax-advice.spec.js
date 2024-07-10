bc8ccb412a28d37981a4295c420b03c596946733
Enon Deogracious Jurence <66213283+Jurence@users.noreply.github.com>
2024-07-10 16:15:28 +0300

    Update tax-advice.spec.js

--- tests/contract-extra-2/tax-advice.spec.js
+++ tests/contract-extra-2/tax-advice.spec.js
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';aaaa
+import { expect, test } from '@playwright/test';aaaaa
 import { fixedContractData } from '../../data/contracts-core/contract-creation';
 import country from '../../data/countries.json';
 import { loginIntoSpecificPage } from '../../helpers/platform/login-ui';
